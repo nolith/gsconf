fa42b5805b6d7fc25b56b69a78717299122f8f76
Alessio Caiazza <nolith@abisso.org>
2013-03-29 11:57:26 +0100

    Allow the user to specify the ethernet device on command line
    If no option are given gsconf will use eth0 as before, but if there's a
    parameter on the command line than the first one will be used as device name

--- main.c
+++ main.c
@@ -11,12 +11,15 @@ int shellHandler = 0;
 
 
 
-int main () {
+int main (int argc, char** argv) {
         int n;
         
         init_socket();
         
-        myMac = get_mac("eth0");
+        char* device = "eth0";
+        if(argc > 1)
+                device = argv[1];
+        myMac = get_mac(device);
         
         gs105e_init();
 
